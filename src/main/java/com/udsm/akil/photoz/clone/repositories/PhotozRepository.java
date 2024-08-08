23b14d47911d5afa020b54b8c44916dd16d49922
AK1239 <akilkhatri235@gmail.com>
2024-08-08 15:41:23 +0300

    updated

---
+++ src/main/java/com/udsm/akil/photoz/clone/repositories/PhotozRepository.java
@@ -0,0 +1,7 @@
+package com.udsm.akil.photoz.clone.repositories;
+
+import com.udsm.akil.photoz.clone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotozRepository extends CrudRepository <Photo,Integer> {
+}
